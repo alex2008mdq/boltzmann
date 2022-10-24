d4c94f255948d3e63d2d085f1ce13662e1ad0664
alex2008mdq <aleparise@gmail.com>
2022-10-23 23:30:49 -0300

    primer prueba

---
+++ demomanim.py
@@ -0,0 +1,12 @@
+from manim import *
+class PrimerScript(Scene):
+    def construct(self):
+        cuadrado = Square()
+        circuloazul = Circle(color=BLUE).shift(LEFT*2)
+        self.add(cuadrado)
+        self.play(Create(circuloazul))
+        self.wait()
+        self.play(circuloazul.animate.set_color(GREEN), Rotate(cuadrado))
+        self.play(FadeOut(circuloazul))
+        self.play(ReplacementTransform(cuadrado, Triangle()))
+        self.wait(2)
